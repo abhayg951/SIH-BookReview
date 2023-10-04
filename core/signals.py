0ac3d50b4cc8ed0641a12661fb34aa31ac1edc53
Abhay Gupta <109977125+abhayg951@users.noreply.github.com>
2023-10-04 22:41:34 +0530

    slug increase

--- core/signals.py
+++ core/signals.py
@@ -8,10 +8,11 @@ import string
 
 @receiver(pre_save, sender=Book)
 def add_slug(sender, instance, *args, **kwargs):
-    n = 10
+    n = 20
 
     if instance and not instance.slug:
         string1 = slugify(instance.name)
-        string2 = slugify(''.join(random.choices(string.ascii_lowercase + instance.name +
+        string2 = slugify(''.join(random.choices(string.ascii_lowercase + string.ascii_uppercase +
+                                                 string.ascii_letters +
                                                  string.digits, k=n)))
         instance.slug = string1 + string2
